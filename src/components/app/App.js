19e17920c2250afd0bf9e167e41be4800355479e
Rohit Anand <rohitanand3456@gmail.com>
2020-08-07 03:09:24 +0530

    Added state for the only available option.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,33 +1,49 @@
-import React, { useState } from 'react';
-import Header from '../header/Header';
+import React, { useState } from "react";
+import Header from "../header/Header";
 import MainDisplay from "../mainDisplay/MainDisplay";
 import PaginationTab from "../paginationTab/PaginationTab";
 
-import Classes from './App.module.scss';
+import Classes from "./App.module.scss";
 import * as utility from "../../utility/utility";
 
 const PRODUCTS_PER_PAGE = 9;
+utility.filterOnlyAvailable();
 
 const App = () => {
-
   const [currPage, setCurrPage] = useState(1);
+  const [OnlyAvailable, setOnlyAvailable] = useState(false);
 
   const updateCurrPageFunc = (pPage) => {
     setCurrPage(pPage);
-  }
+  };
 
   const updateDataForCurrPage = () => {
-    const dataForCurrentPage = utility.pageData(currPage, PRODUCTS_PER_PAGE);
+    const dataForCurrentPage = utility.pageData(
+      currPage,
+      PRODUCTS_PER_PAGE,
+      OnlyAvailable
+    );
     return dataForCurrentPage;
-  }
+  };
+
+  const changeAvailable = () => {
+    OnlyAvailable ? setOnlyAvailable(false) : setOnlyAvailable(true);
+  };
 
   return (
     <div className={Classes.App}>
-      <Header />
+      <Header
+        changeAvailable={changeAvailable}
+        OnlyAvailable={OnlyAvailable}
+      />
       <MainDisplay data={updateDataForCurrPage()} />
-      <PaginationTab totalPages={utility.totalLengthOfData / PRODUCTS_PER_PAGE} currentPage={currPage} updateCurrentPage={updateCurrPageFunc} />
+      <PaginationTab
+        totalPages={utility.totalLengthOfData / PRODUCTS_PER_PAGE}
+        currentPage={currPage}
+        updateCurrentPage={updateCurrPageFunc}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default App;
