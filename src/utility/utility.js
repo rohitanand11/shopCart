b8dca1eed75f00bd454244e45a033071fe1383e2
Rohit Anand <rohitanand3456@gmail.com>
2020-08-07 03:06:37 +0530

    modified utility function for return available products.

--- src/utility/utility.js
+++ src/utility/utility.js
@@ -1,15 +1,26 @@
 import {data} from "../data/data";
 
+let OnlyAvailableData = [];
+
+const filterOnlyAvailable = () => {
+    OnlyAvailableData = data.filter((elem)=>elem['avlble']===1)
+};
+
 const sliceArray = (pOffset , pLimit) =>{
     return data.slice(pOffset,pOffset+pLimit)
 };
 
-const pageData = (pCurrPage,pContentPerPage) =>{
+const pageData = (pCurrPage,pContentPerPage,pOnlyAvailable=true) =>{
     const startingIndex = (pCurrPage-1) * pContentPerPage;
     const endingCount = startingIndex + pContentPerPage -1;
-    return data.slice(startingIndex,endingCount+1);
+
+    if(pOnlyAvailable===true) {
+        return OnlyAvailableData.slice(startingIndex,endingCount+1);
+    } else {
+        return data.slice(startingIndex,endingCount+1);
+    }
 }
 
 const totalLengthOfData = data.length;
 
-export {sliceArray,pageData,totalLengthOfData};
+export {sliceArray,pageData,totalLengthOfData,filterOnlyAvailable};
