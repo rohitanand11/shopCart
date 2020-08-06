ce28352cb790eff2aaadfd4e6c99f243937ef1e1
Rohit Anand <rohitanand3456@gmail.com>
2020-08-07 03:08:01 +0530

    added show only Available product tab on the header.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import Classes from './Header.module.scss';
+import React from "react";
+import Classes from "./Header.module.scss";
 
-const Header = () => {
-    return(
-        <div className={Classes.Header}>
-            <span className={Classes.BrandName}>
-                Shop Cart
-            </span>
-        </div>
+const Header = (props) => {
+
+  const availableCheck = () => {
+    return props.OnlyAvailable ? (
+      <div
+        class={Classes.Available}
+        style={{ backgroundColor: "green",color:"whitesmoke" }}
+        onClick={props.changeAvailable}
+      >
+        filter available
+      </div>
+    ) : (
+      <div class={Classes.Available} onClick={props.changeAvailable}>
+        filter available
+      </div>
     );
-}
+  };
+
+  return (
+    <div className={Classes.Header}>
+      <span className={Classes.BrandName}>Shop Cart</span>
+      {availableCheck()}
+    </div>
+  );
+};
 
 export default Header;
